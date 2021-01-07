a529d2732c8f044f76e47e00723ae1ee3fbb2cee
Kyrylo Rozumovskyi <nortforce@gmail.com>
2021-01-07 11:22:32 +0200

    Reformat file with prettier

--- index.js
+++ index.js
@@ -5,13 +5,12 @@
 // MyArray.isMyArray(arg);  // подсказка: instanceof
 
 function MyArray() {
-
   this.length = 0;
   this.isMyArray = function isMyArray(arg = this) {
     return MyArray.prototype === arg.__proto__;
-  }
+  };
 
-  for(let i = 0; i < arguments.length; i++) {
+  for (let i = 0; i < arguments.length; i++) {
     this.push(arguments[i]);
   }
 }
@@ -25,26 +24,23 @@ function MyArray() {
 // MyArray.prototype.reverse();
 
 function MyArrayPrototype() {
-
   this.push = function () {
-    
     for (let i = 0; i < arguments.length; i++) {
       this[this.length++] = arguments[i];
     }
     return this.length;
-  }
+  };
 
   this.pop = function () {
     if (this.length === 0) {
       return;
     }
     const lastItem = this[this.length - 1];
-    delete this[--this.length]
+    delete this[--this.length];
     return lastItem;
-  }
+  };
 
   this.unshift = function () {
-
     const startingLength = this.length - 1;
 
     for (let i = startingLength; i >= 0; i--) {
@@ -53,37 +49,33 @@ function MyArrayPrototype() {
 
     this.length += arguments.length;
 
-    for(let i=0; i< arguments.length; i++) {
+    for (let i = 0; i < arguments.length; i++) {
       this[i] = arguments[i];
     }
     return this.length;
-  }
+  };
 
   this.shift = function () {
-
     if (this.length === 0) {
       return;
-    } 
+    }
 
     const removedItem = this[0];
 
-    
-    
     if (this.length === 1) {
       delete this[0];
-    } else {      
-      for (let i=0; i < this.length - 1; i++) {
-        this[i] = this[i + 1];        
+    } else {
+      for (let i = 0; i < this.length - 1; i++) {
+        this[i] = this[i + 1];
       }
       delete this[this.length - 1];
     }
 
     this.length--;
     return removedItem;
-  }
+  };
 
   this.concat = function () {
-    
     const newArray = new MyArray();
 
     for (let i = 0; i < this.length; i++) {
@@ -91,79 +83,64 @@ function MyArrayPrototype() {
     }
 
     for (let i = 0; i < arguments.length; i++) {
-
-      if(arguments[i] && newArray.isMyArray(arguments[i])) {
-
-          for(let j = 0; j < arguments[i].length; j++) {
-              newArray.push(arguments[i][j]);
-          }
-         
+      if (arguments[i] && newArray.isMyArray(arguments[i])) {
+        for (let j = 0; j < arguments[i].length; j++) {
+          newArray.push(arguments[i][j]);
+        }
       } else {
         newArray.push(arguments[i]);
       }
-
     }
 
     return newArray;
-  }
+  };
 
   this.reverse = function () {
-
-    // Цикл заменяет первый и последний элемент в массиве потом второй и предпоследний и тд. 
-    // В массиве с нечетным количеством элементов он заменит средний элемент сам собой в последней итерации
     for (let i = 0; i < Math.ceil(this.length / 2); i++) {
-
       const savedItem = this[i];
       const deltaI = this.length - 1 - i;
       this[i] = this[deltaI];
       this[deltaI] = savedItem;
-
     }
 
     return this;
-  }
+  };
 
-  this.forEach = function forEach (callback) {
-
+  this.forEach = function forEach(callback) {
     for (let i = 0; i < this.length; i++) {
       callback(this[i], i, this);
     }
-  }
+  };
 
-  this.map = function map (callback) {
-
+  this.map = function map(callback) {
     const newArr = new MyArray();
 
-    for(let i =0; i< this.length; i++) {
+    for (let i = 0; i < this.length; i++) {
       newArr[i] = callback(this[i], i, this);
     }
 
     return newArr;
-  }
+  };
 
   this.flat = function (depth = 1, targetArray = this) {
     const newArray = new MyArray();
 
-    for(let i = 0; i < targetArray.length; i++) {
-      // 1 уровень вложенности
-      if(targetArray.isMyArray(targetArray[i]) && depth > 0) {
-        const nestedArray = targetArray.flat(depth -1 , targetArray[i]);
-        for(let j = 0;j < nestedArray.length; j++){
+    for (let i = 0; i < targetArray.length; i++) {
+      if (targetArray.isMyArray(targetArray[i]) && depth > 0) {
+        const nestedArray = targetArray.flat(depth - 1, targetArray[i]);
+        for (let j = 0; j < nestedArray.length; j++) {
           newArray.push(nestedArray[j]);
         }
-      }
-      else {
+      } else {
         newArray.push(targetArray[i]);
       }
     }
 
     return newArray;
-  }
+  };
 }
 
 MyArray.prototype = new MyArrayPrototype();
-
-
 
 // advanced
 // MyArray.prototype.forEach();
@@ -174,5 +151,5 @@ const arr1 = new MyArray(1, 2, 3);
 const arr2 = new MyArray(true, false, true);
 
 const nested1 = new MyArray(7, 8, 9);
-const nested2 = new MyArray(4, 5, 6 , nested1);
+const nested2 = new MyArray(4, 5, 6, nested1);
 const nest = new MyArray(1, 2, 3, nested2);
